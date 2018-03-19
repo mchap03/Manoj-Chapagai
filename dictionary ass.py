f904294650bbcd5b126e0879b896be4022f687ff
Chapagai <manoj.chapagai@stu.jefferson.kyschools.us>
2018-03-19 09:30:28 -0400

    Manoj chapagai Dictionary assi

---
+++ dictionary ass.py
@@ -0,0 +1,11 @@
+
+# Manoj chapagi dictionary ass
+
+
+Soccer_postions = {
+    "Forward": "Forward are the players who stay at top of the filed and are resonsible to socre",
+    "Midfielder": "Midfielder are key player that makes the play good by giving the ball through everyone",
+    "Defender": " Defender are player who prevent opposing team to socre",
+    }
+
+
